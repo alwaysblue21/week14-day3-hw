c9228274e3b9e1f61338f74e15f81caa5c3b6051
Je Yun <alwaysblue21@gmail.com>
2023-08-04 16:11:23 -0400

    some more display

--- src/components/Display.js
+++ src/components/Display.js
@@ -8,21 +8,29 @@ function Display(props) {
 
     const [hideAnswer, setHideAnswer] = useState(true)
 
+   if (question[0]) {
     return (
-        <div>
-        <h1>Welcome To Jeopardy!</h1>
-        <h2>Score: {score}</h2>
-        <section>
-            <button onClick={() => setScore(score + 100)}>+</button>
-            <button onClick={() => setScore(score - 100)}>-</button>
-            <button onClick={() => setScore(0)}>Reset to zero</button>
-        </section>
-        <h2>Let's Play!</h2>
-        <section>
-            <button>Get Question</button>
-        </section>
+        <div className="Display">
+            <div>
+                <div className="Score">
+                    <h1>Score: {score}</h1>
+                    <div>
+                        <button className="Score-Button" onClick={() => setScore(score + 100)}>+100 points!</button>
+                        <button className="Score-Button" onClick={() => setScore(score - 100)}>-100 points!</button>
+                        <button className="Reset_button" onClick={() => setScore(0)}>Reset Your SCore to 0!</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="Question-Body">
+                <p>Question: {question[0].question}</p>
+                <p>Category: {question[0].category.title}</p>
+                <p>Points: {question[0].value}</p>
+            </div>
+
         </div>
     )
+   }
 }
 
 export default Display;
