cb9fbce2677ec9fd384bd7add56ccd5cba6d38e8
Je Yun <alwaysblue21@gmail.com>
2023-08-04 16:44:13 -0400

    some fixes

--- src/components/Display.js
+++ src/components/Display.js
@@ -6,8 +6,10 @@ function Display(props) {
 
     const [score, setScore] = useState(0);
 
+    const [hideQuestion, setHideQuestion] = useState(true)
     const [hideAnswer, setHideAnswer] = useState(true)
 
+    const clickQuestion = () => setHideQuestion(!hideQuestion)
     const clickAnswer = () => setHideAnswer(!hideAnswer)
 
    if (question[0]) {
@@ -20,20 +22,20 @@ function Display(props) {
                     <div>
                         <button className="Score-Button" onClick={() => setScore(score + 100)}>+100 points!</button>
                         <button className="Score-Button" onClick={() => setScore(score - 100)}>-100 points!</button>
-                        <button className="Reset_button" onClick={() => setScore(0)}>Reset Your SCore to 0!</button>
+                        <button className="Reset_button" onClick={() => setScore(0)}>Reset Your Score to 0!</button>
                     </div>
                 </div>
             </div>
 
             <div className="Question-Body">
-                <p>Question: {question[0].question}</p>
+                <p><button className="Question-Button" onClick={clickQuestion}>{hideQuestion ? "Get Question" : "Hide Question"}</button></p>
+                <p>Question: {!hideQuestion ? question[0].question : null}</p>
                 <p>Category: {question[0].category.title}</p>
                 <p>Points: {question[0].value}</p>
             </div>
 
             <div className="Question-Body">
-                <h1>Answer: <button className="Answer-Button" onClick={clickAnswer}>{hideAnswer ? "Reveal Question" : "Hide Queestion"}</button></h1>
-                <h1>{!hideAnswer ? question[0].answer : null}</h1>
+                <h1>Answer: <br/> {!hideAnswer ? question[0].answer : null} <br/> <button className="Answer-Button" onClick={clickAnswer}> {hideAnswer ? "Reveal Answer" : "Hide Answer"}</button></h1>
             </div>
 
         </div>
